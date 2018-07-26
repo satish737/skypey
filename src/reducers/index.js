2abc1650fb4fe82b657c7945263f37e741215a39
satish737 <41391352+satish737@users.noreply.github.com>
2018-07-26 12:06:12 -0700

    more adds

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,13 @@ import user from "./user";
 import contacts from "./contacts";
 import { combineReducers } from "redux";
 import activeUserId from "./activeUserId";
+import chat from "./chat";
+import typing from "./typing";
 
 export default combineReducers({
     user,
     contacts,
-    activeUserId
+    activeUserId,
+    chat,
+    typing
   });
